e96261ee1a1487844b1efdd9e454125e92332e44
Vasyl Chura <vasil.chura@gmail.com>
2020-07-10 13:44:33 +0300

    now fixture support all languages of the website

--- conftest.py
+++ conftest.py
@@ -1,18 +1,18 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default=None,
-                     help="Choose browser language!")
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose language: ru or en")
+
 
 @pytest.fixture(scope="function")
 def browser(request):
-    language = request.config.getoption("language")
-    #browser = None
-    browser = webdriver.Chrome()
-    print("\nstart chrome browser for test..")
-    link = f"http://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/"
-    browser.get(link)
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': request.config.getoption("language")})
+    browser = webdriver.Chrome(options=options)
+
     yield browser
-    print("\nquit browser..")
     browser.quit()

71f87402dc128b82691bfa06c53a252e57afbb3f
Vasyl Chura <vasil.chura@gmail.com>
2020-07-14 16:17:13 +0300

    added new locators for login page tests

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,8 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    LOGIN_BUTTON = (By.CSS_SELECTOR, "[name = 'login_submit']")
+    REGISTER_BUTTON = (By.CSS_SELECTOR, "[name = 'registration_submit']")
+    LOGIN_URL = (By.CSS_SELECTOR, "#login_link")
